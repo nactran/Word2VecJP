c436c9b8971588a3dbab31a0ecb319853fd61db2
Yiruka <zx20161434@outlook.com>
2018-08-22 20:04:18 +0800

    単語のベクトル値を見る

--- nucc_similar.py
+++ nucc_similar.py
@@ -2,7 +2,7 @@
 from gensim.models import word2vec
 model = word2vec.Word2Vec.load("nucc.model")
 
-word = input("Enter a word you want to analyze：");
+word = input("Enter a word you want to analyze：")
 simwords = model.most_similar(positive=[word])
 n = [w[0] for w in simwords]
 print(word, "=", ",".join(n))
