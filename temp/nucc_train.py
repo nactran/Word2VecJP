1b387487b1bf65de4a1931d5536f36acc086136c
Yiruka <zx20161434@outlook.com>
2018-08-25 03:01:38 +0800

    iter:10
    訓練時間は著しく長くなった。頻度の高い単語は効果が良さそうになったが頻度の低い単語は目立つ改善が見えないらしい

--- temp/nucc_train.py
+++ temp/nucc_train.py
@@ -2,7 +2,7 @@ from gensim.models import word2vec
 text_file = "../data/nucc1_99.txt"
 
 data = word2vec.LineSentence(text_file)
-model = word2vec.Word2Vec(data, size=120, window=1, hs=1, min_count=1, sg=1)
+model = word2vec.Word2Vec(data, size=120, window=1, hs=1, min_count=1, sg=1,iter = 10)
 model.save("../output/nucc.model")
 print("Generated\n")
 
