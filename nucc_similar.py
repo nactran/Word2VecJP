9fbdb3016324ed06b99a91ea71efb69b6191590c
Yiruka <zx20161434@outlook.com>
2018-08-25 02:43:07 +0800

    parameter:size = 85

--- nucc_similar.py
+++ nucc_similar.py
@@ -1,6 +1,6 @@
 # 類似する語句を表示 - 2
 from gensim.models import word2vec
-model = word2vec.Word2Vec.load("nucc.model")
+model = word2vec.Word2Vec.load("output/nucc.model")
 
 word = input("Enter a word you want to analyze：")
 simwords = model.most_similar(positive=[word])
--- temp/nucc_train.py
+++ temp/nucc_train.py
@@ -1,8 +1,8 @@
 from gensim.models import word2vec
-text_file = "../output/nucc1_99.txt"
+text_file = "../data/nucc1_99.txt"
 
 data = word2vec.LineSentence(text_file)
-model = word2vec.Word2Vec(data, size=100, window=1, hs=1, min_count=1, sg=1)
+model = word2vec.Word2Vec(data, size=85, window=1, hs=1, min_count=1, sg=1)
 model.save("../output/nucc.model")
 print("Generated\n")
 
